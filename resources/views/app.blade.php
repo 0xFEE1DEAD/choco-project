66a4eb7cb60ca7fa74ddfacf2d4a882b1d11b0e7
srv-admin <srv-admin@choco-manage-srv.Home>
2022-07-10 21:36:04 +0800

    some fixes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-    <script src="{{ mix('/js/appv1.js') }}" defer></script>
+    <script src="{{ mix('/js/appv1.1.js') }}" defer></script>
     @inertiaHead
   </head>
   <body style="padding: 0; margin: 0">
