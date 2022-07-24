54bd70f1fc9d853df51abf165574e61b986e7d58
srv-admin <srv-admin@choco-manage-srv.Home>
2022-07-24 13:18:18 +0800

    open graph fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,15 @@
 <html lang="{{config('app.locale')}}">
   <head>
     <meta charset="utf-8" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Шоколадное дело">
+    <meta property="og:title" content="Шоколадное дело - курс для начинающих" />
+    <meta property="og:url" content="https://alenabanokina.ru/"/>
+    <meta property="og:locale" content="ru_RU"/>
+    <meta property="og:description" content="Обучение проходит по видео-урокам и с личной, не ограниченной по времени поддержкой от меня в чате Телеграмма.">
+    <meta property="og:image" content="https://alenabanokina.ru/storage/img/social-media.jpg">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/appv1.3.js') }}" defer></script>
